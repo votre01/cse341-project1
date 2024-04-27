2983b0ed673e0fc2a197126cfcda0da627aed412
Trevor <trevormngwenya@gmail.com>
2024-04-27 23:17:44 +0200

    Project1 checkin mongodb setup

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');
+
+const mongodb = require('./data/database');
 const app = express();
 
 app.use('/', require('./routes'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {console.log(`Running on port ${port}`)});
+
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {console.log(`Database is listening and node running on port ${port}`)});  
+  }
+})
+
